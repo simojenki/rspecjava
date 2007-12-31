6247ea53f77ac7dff52d7a08ea161c04e26a01b1
simon <simon@vadar.(none)>
2007-12-31 11:50:48 +1100

    added animals

---
+++ libjava/animals/Animal.java
@@ -0,0 +1,12 @@
+package animals;
+
+import java.util.Set;
+
+public abstract class Animal {
+
+	public boolean eats(String food) {
+		return foodsIEat().contains(food);
+	}
+
+	public abstract Set<String> foodsIEat();
+}
---
+++ libjava/animals/Mouse.java
@@ -0,0 +1,13 @@
+package animals;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class Mouse extends Animal{
+
+	@Override
+	public Set<String> foodsIEat() {
+		return Collections.singleton("cheese");
+	}
+
+}
